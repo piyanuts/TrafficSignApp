0e8a54ca52ea718537ddd30390c006fd484200f7
piyanuts <bep.n@hotmail.com>
2018-04-04 10:48:21 +0700

    Link Compls

--- app/src/main/java/sdu/cs58/pichsinee/trafficesignapp/MainActivity.java
+++ app/src/main/java/sdu/cs58/pichsinee/trafficesignapp/MainActivity.java
@@ -1,6 +1,7 @@
 package sdu.cs58.pichsinee.trafficesignapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(detailIntent);
             }
         });
+
     }//End Oncreate Method
+
+    public void clickLink(View view){
+        Intent linkIntent = new Intent(Intent.ACTION_VIEW);
+        linkIntent.setData(Uri.parse("https://www.dlt.go.th/th/"));
+        startActivity(linkIntent);
+    }//End clickLink Method
 
 }//End Class
